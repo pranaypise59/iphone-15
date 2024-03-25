842ad2225f5afb66fd69ea7bb06c73af66bc8e56
Pranay Pise <Pranaypise59@gmail.com>
2024-03-25 17:47:47 +0530

    added tailwind css and created navbar

---
+++ src/constants/index.js
@@ -0,0 +1,1 @@
+export const navLists = ["Store", "Mac", "iPhone", "Support"];
---
+++ src/utils/index.js
@@ -0,0 +1,8 @@
+import apple from "/assets/images/apple.svg";
+import search from "/assets/images/search.svg";
+import bag from "/assets/images/bag.svg";
+
+
+export const appleImg = apple;
+export const searchImg = search;
+export const bagImg = bag;
