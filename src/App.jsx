af902166a13895c793f352591cb1f94d99acf97d
Pranay Pise <Pranaypise59@gmail.com>
2024-03-25 18:11:56 +0530

    added videos to highlight area.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import Highlights from './components/highlights'
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
 <main className="bg-black">
   <Navbar/>
   <Hero/>
+  <Highlights/>
 </main>
   )
 }
